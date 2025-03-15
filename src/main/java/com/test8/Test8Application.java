f8d1c766afaa36923d79629b0e28d40bea4b2a5c
Manish Gupta <manishgupta23956@gmail.com>
2025-03-15 13:45:12 +0530

    Update Test8Application.java

--- src/main/java/com/test8/Test8Application.java
+++ src/main/java/com/test8/Test8Application.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test8Application {
 
 	public static void main(String[] args) {
+		String name="mike";
+		int x=100;
+		bollean isPresent=false;
+		int y=200;
+		char x='a';
+		int y=200;
+		double z=10.3f;
+		boolean isPresent=false;
+		float m=10.3f;
 		SpringApplication.run(Test8Application.class, args);
 	}
 
